b172af7a6badaaa3c5911e50a35f743810455614
gerow <gerow.mike@gmail.com>
2013-09-13 16:13:01 -0700

    added tests to make sure we move properly when up against a wall

--- src/test/test_game.cc
+++ src/test/test_game.cc
@@ -12,6 +12,18 @@ TEST(GameTest, TestConstructsProperly) {
   ASSERT_EQ(4, g.y2());
 }
 
+TEST(GameTest, TestCopyConstructor) {
+  Game g(5, 5, 1, 2, 3, 4);
+  Game g2(g);
+
+  ASSERT_EQ(5, g2.w());
+  ASSERT_EQ(5, g2.h());
+  ASSERT_EQ(1, g2.x1());
+  ASSERT_EQ(2, g2.y1());
+  ASSERT_EQ(3, g2.x2());
+  ASSERT_EQ(4, g2.y2());
+}
+
 TEST(GameTest, TestMovesUpProperly) {
   Game g(5, 5, 1, 2, 3, 4);
 
@@ -20,6 +32,13 @@ TEST(GameTest, TestMovesUpProperly) {
   ASSERT_EQ(1, g.y1());
   ASSERT_EQ(4, g.x2());
   ASSERT_EQ(4, g.y2());
+}
+
+TEST(GameTest, TestMovesUpProperlyAtWall) {
+  Game g(5, 5, 1, 1, 3, 4);
+
+  g.move(Game::UP);
+  ASSERT_EQ(1, g.y1());
 }
 
 TEST(GameTest, TestMovesRightProperly) {
@@ -32,6 +51,13 @@ TEST(GameTest, TestMovesRightProperly) {
   ASSERT_EQ(5, g.y2());
 }
 
+TEST(GameTest, TestMovesRightProperlyAtWall) {
+  Game g(5, 5, 5, 1, 3, 4);
+
+  g.move(Game::RIGHT);
+  ASSERT_EQ(5, g.x1());
+}
+
 TEST(GameTest, TestMovesDownProperly) {
   Game g(5, 5, 1, 2, 3, 4);
 
@@ -42,6 +68,13 @@ TEST(GameTest, TestMovesDownProperly) {
   ASSERT_EQ(4, g.y2());
 }
 
+TEST(GameTest, TestMovesDownProperlyAtWall) {
+  Game g(5, 5, 5, 5, 3, 4);
+
+  g.move(Game::DOWN);
+  ASSERT_EQ(5, g.y1());
+}
+
 TEST(GameTest, TestMovesLeftProperly) {
   Game g(5, 5, 2, 3, 4, 5);
 
@@ -50,4 +83,11 @@ TEST(GameTest, TestMovesLeftProperly) {
   ASSERT_EQ(3, g.y1());
   ASSERT_EQ(4, g.x2());
   ASSERT_EQ(4, g.y2());
+}
+
+TEST(GameTest, TestMovesLeftProperlyAtWall) {
+  Game g(5, 5, 1, 1, 3, 4);
+
+  g.move(Game::LEFT);
+  ASSERT_EQ(1, g.x1());
 }
