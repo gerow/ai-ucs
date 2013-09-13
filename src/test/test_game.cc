97064e8ea4178a1ca77ee74e77bfd49e258ace06
gerow <gerow.mike@gmail.com>
2013-09-13 16:07:03 -0700

    added tests for all normal movements

--- src/test/test_game.cc
+++ src/test/test_game.cc
@@ -11,3 +11,43 @@ TEST(GameTest, TestConstructsProperly) {
   ASSERT_EQ(3, g.x2());
   ASSERT_EQ(4, g.y2());
 }
+
+TEST(GameTest, TestMovesUpProperly) {
+  Game g(5, 5, 1, 2, 3, 4);
+
+  g.move(Game::UP);
+  ASSERT_EQ(1, g.x1());
+  ASSERT_EQ(1, g.y1());
+  ASSERT_EQ(4, g.x2());
+  ASSERT_EQ(4, g.y2());
+}
+
+TEST(GameTest, TestMovesRightProperly) {
+  Game g(5, 5, 1, 2, 3, 4);
+
+  g.move(Game::RIGHT);
+  ASSERT_EQ(2, g.x1());
+  ASSERT_EQ(2, g.y1());
+  ASSERT_EQ(3, g.x2());
+  ASSERT_EQ(5, g.y2());
+}
+
+TEST(GameTest, TestMovesDownProperly) {
+  Game g(5, 5, 1, 2, 3, 4);
+
+  g.move(Game::DOWN);
+  ASSERT_EQ(1, g.x1());
+  ASSERT_EQ(3, g.y1());
+  ASSERT_EQ(2, g.x2());
+  ASSERT_EQ(4, g.y2());
+}
+
+TEST(GameTest, TestMovesLeftProperly) {
+  Game g(5, 5, 2, 3, 4, 5);
+
+  g.move(Game::LEFT);
+  ASSERT_EQ(1, g.x1());
+  ASSERT_EQ(3, g.y1());
+  ASSERT_EQ(4, g.x2());
+  ASSERT_EQ(4, g.y2());
+}
