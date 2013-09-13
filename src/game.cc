ae2232a003b93428fa53d4177d1c2573ab90c716
gerow <gerow.mike@gmail.com>
2013-09-13 03:02:32 -0700

    finished most of the movement logic for players in the game.
    Need to test it to make sure that it is actually consistent.

--- src/game.cc
+++ src/game.cc
@@ -1,2 +1,50 @@
 #include <game.h>
 
+Game::Game(int w, int h, int x1, int y1, int x2, int y2) :
+  _w(w),
+  _h(h)
+{
+  _players[0][0] = x1;
+  _players[0][1] = y1;
+  _players[1][0] = x2;
+  _players[1][1] = y2;
+}
+
+void Game::move(Game::Move m)
+{
+  move_player(0, m);
+  if (m == Game::RIGHT) {
+    move_player(1, Game::DOWN);
+  } else if (m == Game::LEFT) {
+    move_player(1, Game::UP);
+  } else if (m == Game::DOWN) {
+    move_player(1, Game::LEFT);
+  } else if (m == Game::UP) {
+    move_player(1, Game::RIGHT);
+  }
+}
+
+int Game::move_cost(Game::Move m)
+{
+  int costs[4] = {
+    5, /* RIGHT */
+    4, /* LEFT */
+    6, /* DOWN */
+    3  /* UP */
+  };
+
+  return costs[m];
+}
+
+void Game::move_player(int player, Game::Move m)
+{
+  if (m == Game::RIGHT && _players[player][0] < _w) {
+    _players[player][0]++;
+  } else if (m == Game::LEFT && _players[player][0] > 1) {
+    _players[player][0]--;
+  } else if (m == Game::DOWN && _players[player][1] < _h) {
+    _players[player][1]++;
+  } else if (m == Game::UP && _players[player][1] > 1) {
+    _players[player][1]--;
+  }
+}
--- src/include/game.h
+++ src/include/game.h
@@ -2,8 +2,15 @@
 #define _GAME_H
 
 class Game {
+  public:
+  	Game(int w, int h, int x1, int y1, int x2, int y2);
+    enum Move { RIGHT, LEFT, DOWN, UP };
+    void move(Move m);
+    int move_cost(Move m);
   private:
-    int x, y;
+    int _w, _h;
+    int _players[2][2];
+    void move_player(int player, Move m);
 };
 
 #endif /* _GAME_H */
