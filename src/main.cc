3e932c660e19df1990b2d8c390cb5ede9ad8b136
gerow <gerow.mike@gmail.com>
2013-10-07 06:36:51 -0700

    working on getting astar to work...

--- src/main.cc
+++ src/main.cc
@@ -3,6 +3,40 @@
 #include <boost/shared_ptr.hpp>
 #include <queue>
 
+template <class T>
+void
+solve(T q, boost::shared_ptr<Game> game, int &expanded, boost::shared_ptr<Game> solution)
+{
+  expanded = 0;
+
+  q.push(game);
+
+  while (!q.empty()) {
+    boost::shared_ptr<Game> cur = q.top();
+    q.pop();
+    expanded++;
+
+    if (cur->is_game_won()) {
+      *solution = *cur;
+      return;
+    }
+    Game::Move moves[4] = {Game::RIGHT, Game::LEFT, Game::DOWN, Game::UP};
+    for (int i = 0; i < 4; i++) {
+      boost::shared_ptr<Game> candidate = boost::shared_ptr<Game>(new Game(*cur));
+      if (!candidate->move(moves[i])) {
+        // if it isn't valid continue
+        continue;
+      }
+
+      if (!cur->state_in_history(candidate->x1(), candidate->y1(), candidate->x2(), candidate->y2())) {
+        // if the new state is not in the history then throw
+        // it into the queue
+        q.push(candidate);
+      }
+    }
+  }
+}
+
 int
 main(int argc, char** argv)
 {
@@ -12,12 +46,36 @@ main(int argc, char** argv)
 
   game_priority_queue q;
 
+  euclidian_priority_queue eq;
+  int e_expanded = 0;
+  boost::shared_ptr<Game> e_solution(new Game);
+  canberra_priority_queue cq;
+  int c_expanded = 0;
+  boost::shared_ptr<Game> c_solution(new Game);
+  knight_priority_queue kq;
+  int k_expanded = 0;
+  boost::shared_ptr<Game> k_solution(new Game);
+
+  solve(eq, g, e_expanded, e_solution);
+  solve(cq, g, c_expanded, c_solution);
+  solve(kq, g, k_expanded, k_solution);
+
+  std::cout << "Nodes Expanded\n";
+  std::cout << "Euclidian Canberra Knight\n";
+  std::cout << e_expanded << " " << c_expanded << " " << k_expanded << "\n";
+
+  e_solution->save("output.txt");
+
+  /*
+  // first do euclidian
   q.push(g);
-  while (!q.empty()) {
+  while (!eq.empty()) {
     // grab an element from the queue and pop
     // it off
-    boost::shared_ptr<Game> cur = q.top();
-    q.pop();
+    boost::shared_ptr<Game> cur = eq.top();
+    eq.pop();
+
+    e_expanded++;
 
     // see if it is a solution
     std::cout << "Seeing if ("
@@ -32,7 +90,8 @@ main(int argc, char** argv)
       // if it is, then we can save it to
       // output.txt and return
       std::cout << "Saving trace to output.txt\n";
-      cur->save("output.txt");
+      //cur->save("output.txt");
+      e_solution = cur;
       std::cout << "Done!\n";
       return 0;
     }
@@ -70,4 +129,5 @@ main(int argc, char** argv)
   g->save_no_solution("output.txt");
 
   return 0;
+  */
 }
